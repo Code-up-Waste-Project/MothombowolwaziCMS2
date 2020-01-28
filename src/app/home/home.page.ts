d3358328b1629c26999aeb3625b2260139d8d5b8
Nathi72TcF <tcfworld72@gmail.com>
2020-01-28 10:27:57 +0200

    nathi28jan home fixed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,13 +13,6 @@ import { computeStackId } from '@ionic/angular/dist/directives/navigation/stack-
 export class HomePage implements OnInit {
   
   @ViewChild('barChart', {static: false}) barChart;
-  @ViewChild('barChart1', {static: false}) barChart1;
-  @ViewChild('barChart2', {static: false}) barChart2;
-  
-  inboundweight=0;
- outboundweight=0;
- Reclaimerweight=0;
-  
   bars: any;
   colorArray: any;
   burgercontent: any = document.getElementsByClassName('burgercontent');
@@ -73,21 +66,6 @@ export class HomePage implements OnInit {
   // email:firebase.auth().currentUser.email
     };
   isAdmin: any;
-//inbound
-  inboundGH001;
-  inboundHD001;
-  inboundLD001;
-  inboundLD003;
-  inboundNFAL01;
-  inboundPAP001;
-  inboundPAP003;
-  inboundPAP005;
-  inboundPAP007;
-  inboundPET001;
-  inboundPET003;
-  inboundPET005;
-
-//storage
   GH001storagemass;
   NFAL01storagemass;
   PAP005storagemass;
@@ -138,75 +116,6 @@ export class HomePage implements OnInit {
  inboundweight=0;
     ionViewDidEnter() {
       this.createBarChart();
-     this.createBarChart1();
-     this.createBarChart2();
-
-
-//pulling data
-//inbound
-this.inboundweight =0;
-firebase.firestore().collection('inbounds').get().then(res=>{
-  res.forEach(val=>{
-    console.log('inboundcalculate',val.data().inboundGH001+val.data().inboundHD001+val.data().inboundLD003+val.data().inboundNFAL01+val.data().inboundPAP001+val.data().inboundPAP003+val.data().inboundPAP005 +val.data().inboundPAP007+val.data().inboundPET001+val.data().inboundPET003+val.data().inboundPET005)
-    this.inboundweight =this.inboundweight 
-    +parseFloat(val.data().inboundGH001)+
-    +parseFloat(val.data().inboundHD001) +
-    +parseFloat(val.data().inboundLD001) +
-    +parseFloat(val.data().inboundLD003) +
-    +parseFloat(val.data().inboundNFAL01) +
-    +parseFloat(val.data().inboundPAP001) +
-    +parseFloat(val.data().inboundPAP003) +
-    +parseFloat(val.data().inboundPAP005) +
-    +parseFloat(val.data().inboundPAP007) +
-    +parseFloat(val.data().inboundPET001) +
-    +parseFloat(val.data().inboundPET003) +
-    +parseFloat(val.data().inboundPET005) ;
-  })
-})
-
-//outbound
-this.outboundweight =0;
-firebase.firestore().collection('outbound').get().then(res=>{
-  res.forEach(val=>{
-    console.log(val.data().GH001+val.data().GH001)
-    this.outboundweight =this.outboundweight 
-    +parseFloat(val.data().GH001)
-    +parseFloat(val.data().HD001)
-    +parseFloat(val.data().LD001)
-    +parseFloat(val.data().HD001)
-    +parseFloat(val.data().LD003)
-    +parseFloat(val.data().NFAL01)
-    +parseFloat(val.data().PAP001)
-    +parseFloat(val.data().PAP003)
-    +parseFloat(val.data().PAP007)
-    +parseFloat(val.data().PAP005)
-    +parseFloat(val.data().PET003)
-    +parseFloat(val.data().PET005);
-  })
-})
-
-//reclaimer
-this.Reclaimerweight =0;
-firebase.firestore().collection('reclaimers').get().then(res=>{
-  res.forEach(val=>{
-    console.log(val.data().GH001Mass+val.data().HD001Mass)
-    this.Reclaimerweight =this.Reclaimerweight 
-    +parseFloat(val.data().GH001Mass)
-    +parseFloat(val.data().HD001Mass)
-    +parseFloat(val.data().LD001Mass)
-    +parseFloat(val.data().LD003Mass)
-    +parseFloat(val.data().NFAL01Mass)
-    +parseFloat(val.data().PAP001Mass)
-    +parseFloat(val.data().PAP003Mass)
-    +parseFloat(val.data().PAP005Mass)
-    +parseFloat(val.data().PAP007Mass)
-    +parseFloat(val.data().PEP005Mass)
-    +parseFloat(val.data().PET001Mass)
-    +parseFloat(val.data().PET003Mass);
-  })
-})
-
-    }
 this.inboundweight =0;
 firebase.firestore().collection('inbounds').get().then(res=>{
   res.forEach(val=>{
@@ -251,10 +160,6 @@ firebase.firestore().collection('reclaimers').get().then(res=>{
     }
   ngOnInit() {
     this.menuCtrl.enable(true); // or true
-  
-
-this. inbounddata();
-console.log('iboundssssssssssssssssssss',this.inbounddata())
     this.getMasses();
     console.log(this.getMasses());
   }
@@ -375,7 +280,7 @@ console.log('iboundssssssssssssssssssss',this.inbounddata())
         this.PET001storagemass = element.data().PET001;
         this.PET003storagemass = element.data().PET003;
         this.PET005storagemass = element.data().PEP005;
-        console.log(element);
+        // console.log(element);
       });
       // console.log(this.GH001storagemass);
       // console.log(this.NFAL01storagemass);
@@ -401,65 +306,6 @@ console.log('iboundssssssssssssssssssss',this.inbounddata())
       this.NFAL01 = (String(NFAL01z).substring(0, 6));
     });
   }
-
-  inbounddata() {
-    let totalPaperz = 0;
-    let GH001z;
-    let NFAL01z;
-
-  
-
-
-
-
-
-    
-    this.db.collection('inbound').onSnapshot(snapshot => {
-      snapshot.forEach(element => {
-        this.inboundGH001 = element.data().inboundGH001;
-        this.inboundHD001 = element.data().inboundHD001;
-        this.inboundLD001 = element.data().inboundLD001;
-        this.inboundLD003 = element.data().inboundLD003;
-        this.inboundNFAL01 = element.data().inboundNFAL01;
-        this. inboundPAP001 = element.data().inboundPAP001;
-        this.inboundPAP003 = element.data().inboundPAP003;
-        this. inboundPAP003 = element.data().inboundPAP003;
-        this.inboundPAP007 = element.data().inboundPAP007;
-        this. inboundPET001 = element.data().inboundPET001;
-        this.inboundPET003 = element.data().inboundPET003;
-        this.inboundPET005 = element.data().inboundPET005;
-        console.log(element);
-      });
-      // console.log(this.GH001storagemass);
-      // console.log(this.NFAL01storagemass);
-      // console.log(this.PAP005storagemass);
-      // console.log(this.PAP007storagemass);
-      // console.log(this.PAP001storagemass);
-      // console.log(this.PAP003storagemass);
-      // console.log(this.HD001storagemass);
-      // console.log(this.LD001storagemass);
-      // console.log(this.LD003storagemass);
-      // console.log(this.PET001storagemass);
-      // console.log(this.PET003storagemass);
-      // console.log(this.PET005storagemass);
-
-      totalPaperz = +this.PAP005storagemass + +this.PAP007storagemass + +this.PAP001storagemass + +this.PAP003storagemass;
-      this.Totalpaper = Number(String(totalPaperz).substring(0, 6));
-
-      this.Totalplastic = +this.HD001storagemass + +this.LD001storagemass + +this.LD003storagemass + +this.PET001storagemass +
-      +this.PET003storagemass + +this.PET005storagemass;
-      this.Totalplasticz = (String(this.Totalplastic).substring(0, 6));
-      String(this.Totalplastic).substring(0, 6);
-
-      GH001z = this.GH001storagemass;
-      this.GH001 = (String(GH001z).substring(0, 6));
-      NFAL01z = this.NFAL01storagemass;
-      this.NFAL01 = (String(NFAL01z).substring(0, 6));
-    });
-  }
-
-
-
   /* bar chart */
   createBarChart() {
     this.bars = new Chart(this.barChart.nativeElement, {
@@ -488,67 +334,6 @@ console.log('iboundssssssssssssssssssss',this.inbounddata())
       }
     });
   }
-
-  
-  /* bar chart */
-  createBarChart1() {
-    this.bars = new Chart(this.barChart1.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ['Aluminium', 'Glass', 'Paper(PAP005)', 'Plastic(PET001)',],
-        // labels: ['Aluminium', 'Glass', 'Paper(PAP005)', 'Paper(PAP007)', 'Paper(PAP003)', 'Paper(PAP003)'],
-        datasets: [{
-          label: 'Overall material ',
-          data: [this.NFAL01storagemass, this.GH001storagemass, this.PAP005storagemass, this.PAP007storagemass, this.PAP003storagemass,
-          this.HD001storagemass, this.LD001storagemass, this.LD003storagemass, this.PET001storagemass, this.PET003storagemass, this.PET005storagemass],
-          // data: [this.NFAL01storagemass, this.GH001storagemass, this.PAP005storagemass, this.PAP007storagemass, this.PAP007storagemass, this.PAP003storagemass],
-          backgroundColor: 'green', // array should have same number of elements as number of dataset
-          borderColor: 'red',  // array should have same number of elements as number of dataset
-          borderWidth: 0.1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
-
-    /* bar chart */
-    createBarChart2() {
-      this.bars = new Chart(this.barChart2.nativeElement, {
-        type: 'bar',
-        data: {
-          labels: ['Aluminium', 'Glass', 'Paper(PAP005)', 'Plastic(PET001)',],
-          // labels: ['Aluminium', 'Glass', 'Paper(PAP005)', 'Paper(PAP007)', 'Paper(PAP003)', 'Paper(PAP003)'],
-          datasets: [{
-            label: 'Overall material ',
-            data: [this.NFAL01storagemass, this.GH001storagemass, this.PAP005storagemass, this.PAP007storagemass, this.PAP003storagemass,
-            this.HD001storagemass, this.LD001storagemass, this.LD003storagemass, this.PET001storagemass, this.PET003storagemass, this.PET005storagemass],
-            // data: [this.NFAL01storagemass, this.GH001storagemass, this.PAP005storagemass, this.PAP007storagemass, this.PAP007storagemass, this.PAP003storagemass],
-            backgroundColor: 'green', // array should have same number of elements as number of dataset
-            borderColor: 'red',  // array should have same number of elements as number of dataset
-            borderWidth: 0.1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
-    }
-
-
   Logout() {
     firebase.auth().signOut().then((res) => {
       console.log(res);
