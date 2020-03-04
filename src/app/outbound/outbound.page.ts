7924eddc530a41bf8832a1e7f550b7deb9a9cbc8
fifi <46625623+fenfe@users.noreply.github.com>
2020-03-04 17:27:09 +0200

    outbound update

--- src/app/outbound/outbound.page.ts
+++ src/app/outbound/outbound.page.ts
@@ -27,7 +27,7 @@ export class OutboundPage implements OnInit {
     from: '',
     text: ''
   };
-
+  transtioning: boolean = false;
   // user infor
   admin = [];
   Newadmin = [];
@@ -190,6 +190,7 @@ export class OutboundPage implements OnInit {
     pagination: {
       el: '.swiper-pagination',
       type: 'progressbar',
+    color: 'red !important',
     }
   }
   isBeginning: boolean = false;
@@ -219,6 +220,9 @@ export class OutboundPage implements OnInit {
       this.otherPopup = true;
     }
 
+    animateJs() {
+      this.transtioning = !this.transtioning;
+    }
     showInputs() {
       this.otherPopup = false;
 
